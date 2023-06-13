1b055088aa02b28988a48af8e1c50c176dd716f3
JereDAM <usr3593@salesianos-lacuesta.net>
2023-06-13 10:16:32 +0100

    Fixed(App): Se arregló la indentación del programa

--- src/es/salesianos/App.java
+++ src/es/salesianos/App.java
@@ -7,28 +7,28 @@ import java.io.FileWriter;
 public class App {
      public static void main(String[] args) {
             
-         Console actividad1 = System.console();
-         
-         String texto = actividad1.readLine("Introduzca un texto con un minimo de 30 caracteres: ");
+        Console actividad1 = System.console();
+        
+        String texto = actividad1.readLine("Introduzca un texto con un minimo de 30 caracteres: ");
 
-         if(texto.length() < 30){
-            while (texto.length() < 30) {
-            System.out.println("Te faltaron caracteres: " + (30 - texto.length()));
-            texto = actividad1.readLine("Vuelve a introducirlo: ");
-            }
-         }else{
-            String textoNuevo = texto.toUpperCase().replace(" ", "_"); 
-            System.out.println(textoNuevo);
+        if(texto.length() < 30){
+        while (texto.length() < 30) {
+        System.out.println("Te faltaron caracteres: " + (30 - texto.length()));
+        texto = actividad1.readLine("Vuelve a introducirlo: ");
+        }
+        }else{
+        String textoNuevo = texto.toUpperCase().replace(" ", "_"); 
+        System.out.println(textoNuevo);
 
-            try {
-                FileWriter textoArchivo = new FileWriter("src/es/salesianos/ficheros/Actividad1.txt");
-                textoArchivo.write(textoNuevo);
-                textoArchivo.close();
-            } catch (Exception e) {
-                e.printStackTrace();
-                System.out.println("nada supongo");
-            }
-         }
+        try {
+            FileWriter textoArchivo = new FileWriter("src/es/salesianos/ficheros/Actividad1.txt");
+            textoArchivo.write(textoNuevo);
+            textoArchivo.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("nada supongo");
+        }
+        }
 
     }
 }
